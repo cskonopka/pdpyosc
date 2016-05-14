8248a7caf50eaee97921e320c916553cebe511b0
Christopher Konopka <cskonopka@gmail.com>
2016-05-14 17:13:56 -0400

    initial - python2pd folder

---
+++ python2pd/pdpyosc-python2pd-sendpython.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+import time
+import OSC
+import os
+
+startup = 1
+# Init OSC
+client = OSC.OSCClient()
+client.connect(('127.0.0.1', 9001)) # first argument is the IP of the host, second argument is the port to use
+
+print ("python program started")
+while startup == 1:
+	for num in range(10, 200):
+		client.send(OSC.OSCMessage("/heading", num))	
